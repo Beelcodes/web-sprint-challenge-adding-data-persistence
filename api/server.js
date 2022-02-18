d6eb1d5989d6aa8c83bdc3dc73e3a0d087198579
Carlos Moreno <carlosmorenodominguez1@hotmail.com>
2022-02-18 16:59:13 -0700

    done

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
 // build your server here and require it from index.js
+const express = require('express')
+const projectsRouter = require('./project/router')
+const resourcesRouter = require('./resource/router')
+const tasksRouter = require('./task/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} is not a valid address`})
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({message: ` Error: ${err.message}`})
+})
+
+module.exports = server;
